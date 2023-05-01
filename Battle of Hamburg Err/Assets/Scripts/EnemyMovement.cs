3c79db7f8ffdf9196defb0cff89b8ada429b14f9
UmarMShameem <umarshameem@outlook.co.nz>
2023-05-01 20:22:56 +1200

    Re-added code for speed settings/health bar

--- Battle of Hamburg Err/Assets/Scripts/EnemyMovement.cs
+++ Battle of Hamburg Err/Assets/Scripts/EnemyMovement.cs
@@ -35,10 +35,12 @@ public class EnemyMovement : MonoBehaviour
             target = WaypointsScript.waypoints[wavepointIndex]; 
             
 
-        } else
+        } 
+        else
         {
+            // Destroy enemy and reduce player health by 5% when enemy has reached the end.
             Destroy(gameObject);
-            
+            PlayerStats.ReducePlayerHealth(0.05f);
         }
         
     }
--- Battle of Hamburg Err/Assets/Scripts/PlayerStats.cs
+++ Battle of Hamburg Err/Assets/Scripts/PlayerStats.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     public static int Money;
     public int startMoney = 1000;
+
+    public Image healthBar;
+    public static float health = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +18,15 @@ public class PlayerStats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!health.Equals(healthBar.fillAmount))
+        {
+            healthBar.fillAmount = health;
+        }
+    }
+
+    public static void ReducePlayerHealth(float amount)
+    {
+        health -= amount;
+
     }
 }
