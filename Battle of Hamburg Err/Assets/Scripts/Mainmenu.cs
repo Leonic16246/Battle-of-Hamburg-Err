3bc5e463dfb19c2dd4bab66e9a51cd7220ac7829
Leonic16246 <108039973+Leonic16246@users.noreply.github.com>
2023-05-01 17:30:46 +1200

    Reworked main menu
    Reworked main menu to not require a theme selection scene

--- Battle of Hamburg Err/Assets/Scripts/Mainmenu.cs
+++ Battle of Hamburg Err/Assets/Scripts/Mainmenu.cs
@@ -5,8 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Mainmenu : MonoBehaviour
 {
-       public void PlayGame ()
+
+    public GameObject mainMenuUI, themeMenuUI;
+
+    public void New()
+    {
+        mainMenuUI.SetActive(false);
+        themeMenuUI.SetActive(true);
+    }
+    public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Back()
+    {
+        themeMenuUI.SetActive(false);
+        mainMenuUI.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting");
+        Application.Quit();
     }
 }
