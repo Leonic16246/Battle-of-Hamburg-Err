b95ee0f8a631a3ff8b98dc904e4b47529c6e0daa
UmarMShameem <umarshameem@outlook.co.nz>
2023-06-05 16:32:33 +1200

    Implemented changes to turret attributes.

--- Battle of Hamburg Err/Assets/Scripts/BuildManager.cs
+++ Battle of Hamburg Err/Assets/Scripts/BuildManager.cs
@@ -24,14 +24,27 @@ public class BuildManager : MonoBehaviour
 
     public NodeUI nodeUI;
 
-    // Apply the permanent upgrades to the turret prefabs.
+    // Apply the permanent upgrades to the turret prefabs based on the levels stored.
     private void Start()
     {
-        if (PlayerPrefs.HasKey("test1") && PlayerPrefs.GetInt("test1") == 1)
+        if (!PlayerPrefs.HasKey("burger turret"))
         {
-            Debug.Log("Test: "+ PlayerPrefs.GetInt("test1"));
-            standardTurretPrefab.GetComponent<Turret>().fireRate = 5;
+            PlayerPrefs.SetInt("burger turret", 0);
         }
+
+        if (!PlayerPrefs.HasKey("sushi launcher"))
+        {
+            PlayerPrefs.SetInt("sushi launcher", 0);
+        }
+
+        if (!PlayerPrefs.HasKey("donut laser"))
+        {
+            PlayerPrefs.SetInt("donut laser", 0);
+        }
+
+        UpgradeBurgerTurret(PlayerPrefs.GetInt("burger turret"));
+        UpgradeSushiLauncher(PlayerPrefs.GetInt("sushi launcher"));
+        UpgradeDonutLaser(PlayerPrefs.GetInt("donut laser"));
     }
 
     public bool CanBuild()
@@ -76,4 +89,78 @@ public class BuildManager : MonoBehaviour
         return turretToBuild; 
     }
 
+    // Apply upgrades to the burger turret prefab.
+    void UpgradeBurgerTurret(int level)
+    {
+        Turret burgerTurret = standardTurretPrefab.GetComponent<Turret>();
+        if (level >= 1)
+        {
+            burgerTurret.pelletprefab.GetComponent<Pellet>().damage = 75;
+            if (level >= 2)
+            {
+                burgerTurret.range = 45;
+                if (level == 3)
+                {
+                    burgerTurret.fireRate = 2;
+                }
+            }
+        }
+        else
+        {
+            // Default values
+            burgerTurret.pelletprefab.GetComponent<Pellet>().damage = 50;
+            burgerTurret.range = 25;
+            burgerTurret.fireRate = 1;
+        }
+    }
+
+    // Apply upgrades to the sushi launcher missile prefab.
+    void UpgradeSushiLauncher(int level)
+    {
+        Pellet sushiLauncherMissile = anotherTurretPrefab.GetComponent<Turret>().pelletprefab.GetComponent<Pellet>();
+        if (level >= 1)
+        {
+            sushiLauncherMissile.damage = 150;
+            if (level >= 2)
+            {
+                sushiLauncherMissile.explosionRadius = 20;
+                if (level == 3)
+                {
+                    sushiLauncherMissile.speed = 50;
+                }
+            }
+        }
+        else
+        {
+            // Default values
+            sushiLauncherMissile.damage = 100;
+            sushiLauncherMissile.explosionRadius = 10;
+            sushiLauncherMissile.speed = 30;
+        }
+    }
+
+    // Apply upgrades to the donut laser prefab.
+    void UpgradeDonutLaser(int level)
+    {
+        Turret donutLaser = LaserBeamerPrefab.GetComponent<Turret>();
+        if (level >= 1)
+        {
+            donutLaser.slowAmount = 0.25f;
+            if (level >= 2)
+            {
+                donutLaser.damageOverTime = 55;
+                if (level == 3)
+                {
+                    donutLaser.range = 50;
+                }
+            }
+        }
+        else 
+        {
+            // Default values
+            donutLaser.slowAmount = 0.5f;
+            donutLaser.damageOverTime = 30;
+            donutLaser.range = 30;
+        }
+    }
 }
